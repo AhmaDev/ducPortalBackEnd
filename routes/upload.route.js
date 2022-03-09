989347f100569ad96fedafd617f90cae5894724e
AhmaDev <ahmadwig@gmail.com>
2022-03-09 14:00:06 +0300

    Add ability to download files

--- routes/upload.route.js
+++ routes/upload.route.js
@@ -7,42 +7,45 @@ const dirRoot = require('../app')
 const fs = require('fs');
 var auth = require('../middlewares/auth.middleware');
 
-router.get('/', auth.roles('Website Editor') ,upload.findAll);
-router.post('/', auth.roles('Website Editor') , uploader.array('file', 25), upload.create);
+router.get('/', auth.roles('Website Editor'), upload.findAll);
+router.post('/', auth.roles('Website Editor'), uploader.array('file', 25), upload.create);
 router.get('/:id', upload.findOne);
-router.get('/section/:id', auth.roles('Website Editor') ,upload.findBySectionId);
+router.get('/section/:id', auth.roles('Website Editor'), upload.findBySectionId);
 router.put('/:id', auth.roles('Website Editor'), upload.updateOne);
 router.delete('/:id', auth.roles('Website Editor'), upload.deleteOne);
 
 router.get("/view/:file", function (request, response) {
-    let file = request.params.file;
-    var extension = file.split(".").pop();
-    var tempFile = path.join(__dirname,'..', 'uploads/' + file)
-    console.log(dirRoot);
-    fs.readFile(tempFile, function (err, data) {
-       console.log(err);
-       switch (extension) {
-          case "jpg":
-             contentType = "image/jpg";
-             isImage = 1;
-             break;
-          case "png":
-             contentType = "image/png";
-             isImage = 1;
-             break;
-          case "pdf":
-             contentType = "application/pdf";
-             isImage = 2;
-             break;
-          case "jpeg":
+   let file = request.params.file;
+   var extension = file.split(".").pop();
+   var tempFile = path.join(__dirname, '..', 'uploads/' + file)
+   fs.readFile(tempFile, function (err, data) {
+      switch (extension) {
+         case "jpg":
+            contentType = "image/jpg";
+            isImage = 1;
+            break;
+         case "png":
+            contentType = "image/png";
+            isImage = 1;
+            break;
+         case "pdf":
+            contentType = "application/pdf";
+            isImage = 2;
+            break;
+         case "jpeg":
             contentType = "image/jpeg";
             isImage = 1;
             break;
-       }
-       response.contentType(contentType);
-       response.send(data);
-    });
- });
+      }
+      if (['jpg', 'jpeg', 'png', 'gif', 'pdf', 'mp4', 'mp3'].includes(extension)) {
+         response.contentType(contentType);
+         response.send(data);
+      } else {
+         res.download(tempFile);
+      }
+
+   });
+});
 
 
 module.exports = router;
