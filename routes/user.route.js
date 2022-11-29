331cb6be38d20eacbfeedbfc167c57db21658ecb
AhmaDev <ahmadwig@gmail.com>
2022-11-29 11:22:37 +0300

    fix

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,13 +1,14 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const user = require('../controllers/user.controller');
-var auth = require('../middlewares/auth.middleware');
+const user = require("../controllers/user.controller");
+var auth = require("../middlewares/auth.middleware");
 
-router.get('/', auth.roles('Admin') ,user.findAll);
-router.post('/', auth.roles('Admin') ,user.create);
-router.post('/login', user.login);
-router.get('/:id', user.findOne);
-router.put('/:id', auth.roles('Admin') ,user.updateOne);
-router.delete('/:id', auth.roles('Admin') ,user.deleteOne);
+router.get("/", auth.roles("Admin"), user.findAll);
+router.post("/", auth.roles("Admin"), user.create);
+router.post("/login", user.login);
+router.get("/version", user.version);
+router.get("/:id", user.findOne);
+router.put("/:id", auth.roles("Admin"), user.updateOne);
+router.delete("/:id", auth.roles("Admin"), user.deleteOne);
 
 module.exports = router;
