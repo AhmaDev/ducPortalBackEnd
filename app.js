6806e1db63038247975aa2f9022ffdb739e10433
AhmaDev <ahmadwig@gmail.com>
2023-03-01 15:12:12 +0300

    fix

--- app.js
+++ app.js
@@ -1,34 +1,33 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require('cors');
-var sqlinjection = require('sql-injection');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var cors = require("cors");
+var sqlinjection = require("sql-injection");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/user.route');
-var sectionsRouter = require('./routes/section.route');
-var lessonRouter = require('./routes/lesson.route');
-var postRouter = require('./routes/post.route');
-var uploadRouter = require('./routes/upload.route');
-var staffRouter = require('./routes/staff.route');
-var fileRouter = require('./routes/file.route');
-var pageRouter = require('./routes/page.route');
-var pageImageRouter = require('./routes/pageImage.route');
-var menuRouter = require('./routes/menu.route');
-var gallaryRouter = require('./routes/gallary.route');
-var studentsRouter = require('./routes/student.route');
-
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/user.route");
+var sectionsRouter = require("./routes/section.route");
+var lessonRouter = require("./routes/lesson.route");
+var postRouter = require("./routes/post.route");
+var uploadRouter = require("./routes/upload.route");
+var staffRouter = require("./routes/staff.route");
+var fileRouter = require("./routes/file.route");
+var pageRouter = require("./routes/page.route");
+var pageImageRouter = require("./routes/pageImage.route");
+var menuRouter = require("./routes/menu.route");
+var gallaryRouter = require("./routes/gallary.route");
+var studentsRouter = require("./routes/student.route");
+var request = require("request");
 var app = express();
 
-
-
-
-app.use(logger('dev'));
-app.use(express.json({ limit: '500mb' }));
-app.use(express.urlencoded({ limit: '500mb', extended: true, parameterLimit: 50000 }));
+app.use(logger("dev"));
+app.use(express.json({ limit: "500mb" }));
+app.use(
+  express.urlencoded({ limit: "500mb", extended: true, parameterLimit: 50000 }),
+);
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
 // app.use(sqlinjection);  // add sql-injection middleware here
 
@@ -36,29 +35,39 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Methods",
-    "GET,HEAD,PUT,POST,OPTIONS,UPDATE,DELETE"
+    "GET,HEAD,PUT,POST,OPTIONS,UPDATE,DELETE",
   );
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization, token"
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization, token",
   );
   next();
 });
 
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/sections", sectionsRouter);
+app.use("/lessons", lessonRouter);
+app.use("/posts", postRouter);
+app.use("/uploads", uploadRouter);
+app.use("/staff", staffRouter);
+app.use("/files", fileRouter);
+app.use("/pages", pageRouter);
+app.use("/pageImages", pageImageRouter);
+app.use("/menus", menuRouter);
+app.use("/photos", gallaryRouter);
+app.use("/students", studentsRouter);
+app.use("/", require("./routes/blockReason.route"));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/sections', sectionsRouter);
-app.use('/lessons', lessonRouter);
-app.use('/posts', postRouter);
-app.use('/uploads', uploadRouter);
-app.use('/staff', staffRouter);
-app.use('/files', fileRouter);
-app.use('/pages', pageRouter);
-app.use('/pageImages', pageImageRouter);
-app.use('/menus', menuRouter);
-app.use('/photos', gallaryRouter);
-app.use('/students', studentsRouter);
-app.use('/', require('./routes/blockReason.route'));
+app.get("/years", (req, res) => {
+  var options = {
+    method: "GET",
+    url: "http://109.224.31.12:3100/api/yearStudies",
+  };
 
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+    res.send(body);
+  });
+});
 module.exports = app;
